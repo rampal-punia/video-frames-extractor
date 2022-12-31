7ad984d9add7a20d0fc2fffe6be756a049d0a598
Coding Mantras <rampalsinghpunia@gmail.com>
2022-12-31 14:49:22 +0530

    Update settings and readme

--- README.md
+++ README.md
@@ -18,17 +18,23 @@ pip install imutils
 
 ## Settings
 
+Edit required settings in the `settings.py`
+
 ```python
 # Video file
-VIDEO_DIRPATH = ROOT / 'videos'         # EDIT: 1
-VIDEO_FILE_FULLNAME = 'vid_1.mp4'        # EDIT: 2
+VIDEO_DIRPATH = ROOT / 'videos'             # EDIT: 1 (Required)
+VIDEO_FILE_FULLNAME = 'truck.ts'            # EDIT: 2 (Required)
 
 # Frame settings
-REQUIRED_FRAME_RATE = 2               # EDIT: 3
-START_FROM_SECOND = 1                   # EDIT: 4
-REQUIRED_IMAGE_FORMAT = 'png'          # EDIT: 5
-REQUIRED_IMAGE_WIDTH = 480
+REQUIRED_FRAME_RATE = 2                     # EDIT: 3
+# Number of seconds for a frame. 2 means, 1 frame for 2 seconds
+
+START_FROM_SECOND = 3                       # EDIT: 4
+# start extraction of the frames after 3 seconds of the video is passed.
+
+REQUIRED_IMAGE_FORMAT = 'png'               # EDIT: 5
+REQUIRED_IMAGE_WIDTH = 480                  # EDIT: 6
 
 # Output frames dirpath
-OUTDIR = BASE_ROOT/'vid_1'             # EDIT: 6
+OUTDIR = BASE_ROOT/'truck'                  # EDIT: 7 (Required)
 ```
--- settings.py
+++ settings.py
@@ -10,11 +10,11 @@ if str(BASE_ROOT) not in sys.path:
 ROOT = Path(os.path.relpath(BASE_ROOT, Path.cwd()))
 
 #############################
-### Edit these 6 settings ###
+### Edit these settings ###
 #############################
 # Video file
-VIDEO_DIRPATH = ROOT / 'videos'             # EDIT: 1
-VIDEO_FILE_FULLNAME = 'truck.ts'           # EDIT: 2
+VIDEO_DIRPATH = ROOT / 'videos'             # EDIT: 1 (Required)
+VIDEO_FILE_FULLNAME = 'truck.ts'            # EDIT: 2 (Required)
 
 # Frame settings
 REQUIRED_FRAME_RATE = 2                     # EDIT: 3
@@ -24,10 +24,12 @@ START_FROM_SECOND = 3                       # EDIT: 4
 # start extraction of the frames after 3 seconds of the video is passed.
 
 REQUIRED_IMAGE_FORMAT = 'png'               # EDIT: 5
-REQUIRED_IMAGE_WIDTH = 480
+REQUIRED_IMAGE_WIDTH = 480                  # EDIT: 6
 
 # Output frames dirpath
-OUTDIR = BASE_ROOT/'truck'                  # EDIT: 6
+OUTDIR = BASE_ROOT/'truck'                  # EDIT: 7 (Required)
+
+### END EDIT ###
 
 VIDNAME_WITHOUT_EXT = os.path.splitext(VIDEO_FILE_FULLNAME)[0]
 VIDEO_FILE_PATH = os.path.join(VIDEO_DIRPATH, VIDEO_FILE_FULLNAME)
